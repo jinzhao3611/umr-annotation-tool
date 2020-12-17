2debf7b0265709b060fb9f3cb4f19a7fea96062a
jinzhao <jinzhao3611@gmail.com>
2020-12-17 09:00:44 -0500

    fixed the highlight bug, and break the choices section into 3 columns

--- umr_annot_tool/static/scripts/main.js
+++ umr_annot_tool/static/scripts/main.js
@@ -514,7 +514,6 @@ function color_amr_elem(id, color, event_type) {
 /** entrance ******************************************************/
 // see the original one
 function submit_template_action(id = "nothing", numbered_predicate = "") {
-
     console.log("submit_template_action: id: " + id + ", numbered_predicate: " + numbered_predicate);
     highlightSelection();
     if (id == "nothing") {
@@ -3724,47 +3723,33 @@ document.onselectionchange = function selectSpan() {
 };
 
 function highlightSelection() {
+    console.log("highlightSelection is called.");
     var userSelection = document.getSelection();
 
     //Attempting to highlight multiple selections (for multiple nodes only + Currently removes the formatting)
-    for (var i = 0; i < userSelection.rangeCount; i++) {
-        //Copy the selection onto a new element and highlight it
-        var node = highlightRange(userSelection.getRangeAt(i)/*.toString()*/);
-        // Make the range into a variable so we can replace it
-        var range = userSelection.getRangeAt(i);
-        //Delete the current selection
-        range.deleteContents();
-        //Insert the copy
-        range.insertNode(node);
-    }
-}
-
-//Function that highlights a selection and makes it clickable
-function highlightRange(range) {
-    //Create the new Node
-    var newNode = document.createElement("span");
+    //Copy the selection onto a new element and highlight it
+    // var node = highlightRange(userSelection.getRangeAt(0)/*.toString()*/);
+    // Make the range into a variable so we can replace it
+    var range = userSelection.getRangeAt(0);
+    var startIdx = range.startContainer.parentNode.cellIndex;
+    var endIdx = range.endContainer.parentNode.cellIndex;
+    var tableRow = range.endContainer.parentNode.parentNode;
+    for (let i = startIdx; i <= endIdx; i++) {
+        var newNode = document.createElement("span");
 
     // Make it highlight
-    newNode.setAttribute("class", "text-muted");
-
+        newNode.setAttribute("class", "text-success");
     //Make it "Clickable"
-    newNode.onclick = function () {
-        if (confirm("do you want to delete it?")) {
-            deletenode(newNode);
-        } else {
-            alert(range);
-        }
-    };
-
-
-    //Add Text for replacement (for multiple nodes only)
-    //newNode.innerHTML += range;
-    newNode.appendChild(range.cloneContents());
-
-    //Apply Node around selection (used for individual nodes only)
-    //range.surroundContents(newNode);
-
-    return newNode;
+        newNode.onclick = function () {
+            if (confirm("do you want to delete it?")) {
+                deletenode(newNode);
+            } else {
+                alert(tableRow.cells[i]);
+            }
+        };
+        newNode.appendChild(document.createTextNode(tableRow.cells[i].innerText));
+        tableRow.cells[i].replaceChild(newNode, tableRow.cells[i].firstChild)
+    }
 }
 
 function deletenode(node) {
