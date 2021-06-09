5dce974a621aed9e2c0cffadf5006dcf88385aa1
jinzhao <jinzhao3611@gmail.com>
2021-06-09 19:42:25 -0400

    bug fixed: register bug

--- umr_annot_tool/users/routes.py
+++ umr_annot_tool/users/routes.py
@@ -20,7 +20,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password, lexicon={})
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
