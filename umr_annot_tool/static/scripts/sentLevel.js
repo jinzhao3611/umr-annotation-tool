e4fc8d5198ee95e5089f94accfa1f24ad2d5fb60
jinzhao <jinzhao3611@gmail.com>
2022-02-14 11:15:57 -0500

    fix bug: save not working properly because I renamed annotate to sentlevel earlier

--- umr_annot_tool/static/scripts/sentLevel.js
+++ umr_annot_tool/static/scripts/sentLevel.js
@@ -2687,13 +2687,14 @@ function UMR2db() {
 
     removeDeletedFromUmr();
 
-    fetch(`/annotate/${doc_id}`, {
+    fetch(`/sentlevel/${doc_id}`, {
         method: 'POST',
         body: JSON.stringify({"amr": amrHtml, "align": align_info, "snt_id": snt_id, "umr": umr})
     }).then(function (response) {
         return response.json();
     }).then(function (data) {
         setInnerHTML("error_msg", data["msg"]);
+        document.getElementById("error_msg").className = `alert alert-${data['msg_category']}`;
     }).catch(function(error){
         console.log("Fetch error: "+ error);
     });
