29c02050d64d8bfc4fc94e65734a47601d9f1ed8
jinzhao <jinzhao3611@gmail.com>
2022-10-26 21:20:20 -0400

    able to parse files with existing doc level annotation

--- one_time_scripts/parse_input_xml.py
+++ one_time_scripts/parse_input_xml.py
@@ -43,26 +43,31 @@ def amr_text2html(plain_text: str) -> str:
     html_string = '<div id="amr">' + html_string + '</div>\n'
     return html_string
 
-def process_exported_file_isi_editor(content_string: str) -> Tuple[str, List[List[str]], List[str]]:
+def process_exported_file_isi_editor(content_string: str) -> Tuple[str, List[List[str]], List[str], List[str]]:
     """
     example file: /Users/jinzhao/schoolwork/lab-work/umr_annot_tool_resources/people/kristin/events37.xml
+    /Users/jinzhao/schoolwork/lab-work/umr_annot_tool_resources/people/sijia/events37_wzdoc.xml
     """
     doc_content_string = ""
     sents = []
     sent_annots = []
+    doc_annots = []
     root = ET.fromstring(content_string)
     for child in root:
         if child.tag =='sntamr':
             for child2 in child:
                 if child2.tag == 'amr':
-                    amr = regex.sub(r'(\()([a-z][0-9]? /)', rf'\1s{str(len(sent_annots)+1)}\2', child2.text)
+                    amr = regex.sub(r'(\()([a-z][0-9]? /)', rf'\1s{str(len(sent_annots)+1)}\2', child2.text) #add sentence number to each variable
                     sent_annots.append(amr)
-
+                elif child2.tag == 'doc-amr':
+                    if child2.text:
+                        doc_annots.append(child2.text) #assuming the input file looks like events37.xml sent by Sijia
+                    else:
+                        doc_annots.append("") #otherwise NoneType object will be added in
                 elif child2.tag == 'sentence':
                     doc_content_string += '\n'+child2.text
                     sents.append(child2.text.split())
-
-    return doc_content_string.strip(), sents, sent_annots
+    return doc_content_string.strip(), sents, sent_annots, doc_annots
 
 def parse_exported_file(content_string: str) -> Tuple[str, List[List[str]], List[str], List[str], List[Dict[str, str]]]:
     """
